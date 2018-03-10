edd9911f8b65719fe7ee621d5340b646e21966fa
root <912744609@qq.com>
2018-03-11 07:42:26 +0800

    完成项目初始化

---
+++ common/config/fecshop_local_services/Image.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ *
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+    'image' => [
+        'appbase'    => [
+            'appfront' => [
+                'basedir'    => '@appimage/appfront',
+                'basedomain' => '//im3.sqc666.com',
+            ],
+            'apphtml5' => [
+                'basedir'    => '@appimage/apphtml5',
+                'basedomain' => '//im2.sqc666.com',
+            ],
+            //'appadmin' => [
+            //	'basedir' => '@appimage/appadmin',
+            //	'basedomain' => '//im4.sqc666.com',
+            //],
+            'common' => [
+                'basedir'    => '@appimage/common',
+                'basedomain' => '//im1.sqc666.com',
+            ],
+        ],
+    ],
+];
